265290dba2f38df33112fe89fc69e0b41fee4627
hexoN <aliakbaripour00@gmail.com>
2024-03-03 11:49:56 +0330

    localStorage & new functions Add playGame function Add localStorage to save the data

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-const randomNumber = Math.random();
-let result;
-
-if (randomNumber < 0.5) {
-  result = 'heads';
-} else {
-  result = 'tails';
+let score = JSON.parse(localStorage.getItem('scoreCount')) || {
+  win: 0,
+  lose: 0,
 }
 
-const guess = 'heads';
+function playGame(guess) {
+  const randomNumber = Math.random()
+  let result;
 
-if (guess === result) {
-  console.log('You won;');
-} else {
-  console.log('You lose;');
+  if (randomNumber < 0.5) {
+    result = 'heads'
+  } else {
+    result = 'tails'
+  }
+
+  if (guess === result) {
+    score.win++
+    localStorage.setItem('scoreCount', JSON.stringify(score))
+    alert(`You won\nWins: ${score.win} Loses: ${score.lose}`)
+  } else {
+    score.lose++
+    localStorage.setItem('scoreCount', JSON.stringify(score))
+    alert(`You lose\nWins: ${score.win} Loses: ${score.lose}`)
+  }
 }
